d1a3c69665383d33dfbea62aaf52739860c0c61a
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-09-09 13:35:05 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+}
+
+module.exports = nextConfig
